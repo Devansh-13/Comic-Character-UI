391ef1a572273e20cd4a3d3b90f2af010f55ba9b
Devansh-13 <devansh15091970@gmail.com>
2024-03-21 17:11:25 +0530

    Comic frontend

---
+++ fetch.js
@@ -0,0 +1,7 @@
+
+export async function fetchApi(){
+    const pr=await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=72da67d62f45f9ac54afe918dbea3531&hash=d6e4c30b438da847381aea1539e005dd");
+    const data=await pr.json();
+    return data; 
+}
+
