c987a90100c15ba168d008e9e4b5741ee8228e88
Devansh-13 <devansh15091970@gmail.com>
2024-03-25 17:38:24 +0530

    Search Button

--- fetch.js
+++ fetch.js
@@ -5,3 +5,8 @@ export async function fetchApi(){
     return data; 
 }
 
+export async function fetchChar(charName){
+    const pr=await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${charName}&ts=1&apikey=72da67d62f45f9ac54afe918dbea3531&hash=d6e4c30b438da847381aea1539e005dd`);
+    const data=await pr.json();
+    return data; 
+}
